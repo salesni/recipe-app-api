7ae3a5bf3115523281e4db3294b3483772c5a8ab
MyName <fuensal98@gmail.com>
2021-05-06 17:04:05 -0500

    Add Ingredient API

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='test@tes.com', password='testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email,password)
+
 class ModelTests(TestCase):
     
     def test_create_user_with_email_successful(self):
@@ -36,3 +43,20 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tah in string representation"""
+        tag = models.Tag.objects.create(
+            user = sample_user(),
+            name = 'Vegan'
+        ) 
+        self.assertEqual(str(tag), tag.name)
+    
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name = 'Cucumber',
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
