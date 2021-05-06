77dfe4140e98d5f895685e976cd0850cba3a79c7
MyName <fuensal98@gmail.com>
2021-05-06 12:12:23 -0500

    Update User with Token

--- app/user/views.py
+++ app/user/views.py
@@ -15,3 +15,12 @@ class CreateTokenView(ObtainAuthToken):
     #Set the renderer in order to see the API on the browser
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return authentication user"""
+        return self.request.user
